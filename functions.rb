1e4945cd13bb9f703160333a7e560c2d89f597a5
Stephanie Michelle Moore <smoore26@users.noreply.github.com>
2014-03-22 03:40:36 -0400

    Update functions.rb
    implementing puts reverse([3,6,'dog']).inspect

--- functions.rb
+++ functions.rb
@@ -28,7 +28,7 @@ end
 
 
 # Uncomment each of these to test your functions
-# puts reverse([3,6,'dog']).inspect
+puts reverse([3,6,'dog']).inspect
 # puts histogram('The Quick brown fox').inspect
 # puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
 # puts fizzbuzz.join("\n")
