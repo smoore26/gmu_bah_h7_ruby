cd5652f4728a9d2bf87550798b30c11b16483e4a
Stephanie Moore <blessedsmc2002@yahoo.com>
2014-04-15 22:16:00 -0400

    HW7

--- functions.rb
+++ functions.rb
@@ -16,7 +16,7 @@ end
 # (Hint: the is_a? method might be useful here)
 # 
 def sum_only_numbers(an_array)
-	    an_array.select{|x| (x.is_a? numeric)}.inject{|ct,x| ct+x}
+	    an_array.select{|x| (x.is_a? Numeric)}.inject{|ct,x| ct+x}
 end
 
 # For i from 1 to 100, return an array.
@@ -27,26 +27,24 @@ end
 # Otherwise, the element is simply the value of i
 # For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz', ...]
 def fizzbuzz
-	    array = Array.new
-	    for i in (1..100)
-	    	     if i%3 == 0
-	    	     	    array.push("Fizz")
-                 elsif i%5 == 0
-                         array.push("Buzz")
-                 elsif i%3 == 0 && i%5 == 0
-                 	     array.push("FizzBuzz")
-                 else
-                 	     aray.push(i)
-                 end
-        end
-        return array
-end        	     
-
+	array = []
+	for i in (1..100)
+		if i%3 == 0 && i%5 == 0
+			     array.push("FizzBuzz")
+	    elsif i%5 == 0
+		     array.push("Buzz")
+		elsif i%3 == 0
+			    array.push("Fizz")
+		else
+			     array.push(i)
+	    end
+	end        	     
+	array
 end
 
 
 # Uncomment each of these to test your functions
 puts reverse([3,6,'dog']).inspect
 # puts histogram('The Quick brown fox').inspect
-# puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
-# puts fizzbuzz.join("\n")
+puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
+puts fizzbuzz.join("\n")
